f5b92deb97ca0d61a2907a429843233407e76b0f
xiong <3372998125@qq.com>
2022-08-05 11:18:53 +0800

    first commit

---
+++ src/test/java/com/bao/campusportal/CampusportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bao.campusportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
